9e6d5f035c061c6d84316c8d811ca6b8e4c6a439
saurabh mishra <smishra43@hotmail.com>
2021-04-05 17:04:00 +0530

    Add project files.

---
+++ ConvertSortedArrayToBinarySearchTree_LeetCode/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertSortedArrayToBinarySearchTree_LeetCode
+{
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+
+        public Node(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+
+        }
+    }
+    public class BinaryTree
+    {
+        Node root;
+
+        public Node ConvertToBST(int[] arr,int start,int end)
+        {
+            if (start > end)
+                return null;
+
+            int mid = (start + end) / 2;
+            Node node = new Node(arr[mid]);
+
+            node.left = ConvertToBST(arr, start, mid - 1);
+            node.right = ConvertToBST(arr, mid + 1, end);
+
+
+            return node;
+        }
+
+        public void preOrder(Node node)
+        {
+            if (node == null)
+                return;
+            Console.Write(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BinaryTree bt = new BinaryTree();
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int n = arr.Length;
+            
+            Node resultRoot=bt.ConvertToBST(arr, 0, n - 1);
+            bt.preOrder(resultRoot);
+
+            Console.ReadLine();
+
+        }
+    }
+}
